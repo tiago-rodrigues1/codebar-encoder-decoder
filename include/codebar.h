dcdde9be0bfd5acd8202b1c1a98fdcbe6d684718
tiago-rodrigues1 <tiagords127@gmail.com>
2024-12-31 15:10:29 -0300

    feat: add struct CodigoDeBarras

--- include/codebar.h
+++ include/codebar.h
@@ -6,5 +6,17 @@
 #define TAM_CODIGO_DE_BARRAS 67
 #endif
 
+#ifndef CODIGO_DE_BARRAS
+#define CODIGO_DE_BARRAS
+typedef struct codigo {
+    int identificador;
+    int pxMargem;
+    int pxPorArea;
+    int pxAltura;
+    char *nome;
+    char binario[TAM_CODIGO_DE_BARRAS];
+} CodigoDeBarras;
+#endif
+
 int isDigitoVerificadorValido(int identificador);
 void getBinario(int identificador, char stringBinario[TAM_CODIGO_DE_BARRAS]);
