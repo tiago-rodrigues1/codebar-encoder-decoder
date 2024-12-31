862cb37efce185ba1d741c1b8669907f991d4219
tiago-rodrigues1 <tiagords127@gmail.com>
2024-12-31 16:01:29 -0300

    feat(wip): add encoder

--- encoder.c
+++ encoder.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include "include/codebar.h"
+#include "include/pbm.h"
+
+
+int main(void) {
+    CodigoDeBarras c;
+
+    c.identificador = 40170725;
+    c.pxPorArea = 3;
+    c.pxMargem = 8;
+    c.pxAltura = 180;
+
+    c.nome = "teste.pbm";
+    
+    char binario[TAM_CODIGO_DE_BARRAS];
+    getBinario(c.identificador, binario);
+
+    printf("%s\n", binario);
+
+    strcpy(c.binario, binario);
+
+    gerarPBM(&c);
+    
+    return 0;
+}
