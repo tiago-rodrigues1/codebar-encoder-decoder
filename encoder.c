6505060057ffd20af4422bdeac441ca75eabce6d
deborahmacedo <deborah.macedo.104@ufrn.br>
2025-01-05 14:37:33 -0300

    Entrada e verificar se tem algo sem ser numero

--- encoder.c
+++ encoder.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "include/codebar.h"
+#define TAM 8
+
+int main(){
+    char codigo[TAM];
+    int i;
+//pedir codigo
+    printf("Digite o código: \n ");
+    scanf("%s", codigo);
+
+//verificar se tem algo q n é numero
+
+    for (i = 0; codigo[i] != '\0'; i++) {
+        if (!isdigit(codigo[i])) { 
+            printf("Há caractere inválido");          
+            break;                 
+        }
+    }
+
+//verificar se tem a quantidade certa de caracteres (8)
+
+
+
+
+//verificar se está lendo ok
+    //printf("O código é esse: %s", codigo);
+
+return 0;
+}
