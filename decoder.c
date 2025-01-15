d423ecb60f5a19549a48b21cddf84e3689e7d2a4
tiago-rodrigues1 <tiagords127@gmail.com>
2025-01-14 23:30:37 -0300

    (wip) decoder

--- decoder.c
+++ decoder.c
@@ -2,19 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "include/codebar.h"
+#include "include/pbm.h"
+
 void usage() {
     printf("Modo de uso:\n");
     printf("$ ./decoder <nome_do_arquivo>\n");
 }
 
-int main(int argc, char* argv[]) {
-    char *path;
-    path = argv[1];
+int main() {
+    // char *path;
+    // path = argv[1];
 
-    if (strcmp(argv[1], "-h") == 0 || argv[1] == NULL) {
-        usage();
-        exit(-1);
-    }
+    // if (strcmp(argv[1], "-h") == 0 || argv[1] == NULL) {
+    //     usage();
+    //     exit(-1);
+    // }
+
+    // int x = getDecimal("1010011001001001101111010100011010101001110101000010001001110010101");
+
+    // int existe = pbmValido("codigo.pbm");
+    // pbmValido("codigo.pbm");
+    // printf("---> %d\n", existe);
+
+    char stringBinario[TAM_CODIGO_DE_BARRAS];
+
+    extrairCodigoBinario("tiago.pbm", stringBinario);
+
+    printf("%s\n", stringBinario);
 
     return 0;
 }
